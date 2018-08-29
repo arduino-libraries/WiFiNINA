f01e9c3d607732ee223761d6d6409458cb872fea
Sandeep Mistry <s.mistry@arduino.cc>
2018-08-29 14:57:18 -0400

    Rename WiFiNINAFirmwareUpdater to SerialNINAPassthrough

--- examples/Tools/SerialNINAPassthrough/SerialNINAPassthrough.ino
+++ examples/Tools/SerialNINAPassthrough/SerialNINAPassthrough.ino
@@ -1,5 +1,5 @@
 /*
-  WiFiNINAFirmwareUpdater - Firmware Updater for the 
+  SerialNINAPassthrough - Use esptool to flash the u-blox NINA (ESP32) module
   Arduino MKR WiFi 1010, Arduino MKR Vidor 4000, and Arduino UNO WiFi Rev.2.
   
   Copyright (c) 2018 Arduino SA. All rights reserved.
