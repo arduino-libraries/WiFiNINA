5f89952fde3dce3041635767b1b3a2855115b806
Martino Facchin <m.facchin@arduino.cc>
2019-08-14 09:29:26 +0200

    Fix WPA2 Enterprise example

--- examples/WiFiWebClientWPA2Enterprise/WiFiWebClientWPA2Enterprise.ino
+++ examples/WiFiWebClientWPA2Enterprise/WiFiWebClientWPA2Enterprise.ino
@@ -1,65 +1,69 @@
-
 /*
-  Web client
+  Web client using WPA2 Enterprise connection
 
- This sketch connects to a website (http://www.google.com)
- using the WiFi module.
+  This sketch connects to a website (http://www.arduino.cc)
+  using the WiFi module.
 
- This example is written for a network using WPA encryption. For
- WEP or WPA, change the WiFi.begin() call accordingly.
+  This example is written for a network using WPA2 Enterprise encryption.
 
- Circuit:
- * Board with NINA module (Arduino MKR WiFi 1010, MKR VIDOR 4000 and UNO WiFi Rev.2)
+  Circuit:
+   Board with NINA module (Arduino MKR WiFi 1010, MKR VIDOR 4000 and UNO WiFi Rev.2) with firmware > 1.2.4
 
- created 13 July 2010
- by dlf (Metodo2 srl)
- modified 31 May 2012
- by Tom Igoe
- */
+  created 13 July 2010
+  by dlf (Metodo2 srl)
+  modified 31 May 2012
+  by Tom Igoe
+  modified 14 Aug 2019
+  by Martino Facchin
+*/
 
 
 #include <SPI.h>
 #include <WiFiNINA.h>
 
-#include "arduino_secrets.h" 
+#include "arduino_secrets.h"
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
 char ssid[] = SECRET_SSID;        // your network SSID (name)
-char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 int keyIndex = 0;            // your network key Index number (needed only for WEP)
 
 int status = WL_IDLE_STATUS;
-// if you don't want to use DNS (and reduce your sketch size)
-// use the numeric IP instead of the name for the server:
-//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
-char server[] = "www.google.com";    // name address for Google (using DNS)
+char server[] = "www.arduino.cc";    // name address for Arduino.cc (using DNS)
 
-// Initialize the Ethernet client library
-// with the IP address and port of the server
-// that you want to connect to (port 80 is default for HTTP):
+// Initialize the WiFi Client
 WiFiClient client;
 
-// Initialize the WPA2 Enterprise object
-// Just fill the fields you need
-// eg. if you are given only an identity and a certificate, initialize it as 
-//    WPA2Enterprise data("myidentity", ca_pem);
-// Certificates are stored in the Secret tab to avoid sharing them.
+// WPA2 Enterprise usage:
 
-WPA2Enterprise data(EAP_TLS, "myidentity", "username", "password", ca_pem, client_crt, client_key);
+// Certificates and credentials are stored in secret tab to avoid sharing them.
+// For some networks (like eduroam) your configuration will simply look like
+//
+// #define SECRET_SSID     "eduroam"
+// #define SECRET_USERNAME "identity@youruniversity.domain"
+// #define SECRET_PASS     "yourpassword"
+//
+// Call WiFi.beginEnterprise(SECRET_SSID, SECRET_USERNAME, SECRET_PASS) and you should easily connect
+//
+// In case of more complicated networks (that require a certificate, for example) you can use
+// WPA2Enterprise functions
+// WPA2Enterprise.addCACertificate() , addClientCertificate(), addIdentity() ...
+// and then call WiFi.begin() as usual.
 
 void setup() {
-  //Initialize serial and wait for port to open:
+  // Initialize serial and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
+    ; // Wait for serial port to connect. Needed for native USB port only
   }
 
-  // configure the WiFi module to use provided WPA2 Enterprise parameters
-  WiFi.config(data);
+  // Configure the wifi module to use provided WPA2 Enterprise parameters (if needed)
+  Serial.println("Configuring WPA2 Enterprise credentials");
+  WPA2Enterprise.addCACertificate(ca_pem);
+  WPA2Enterprise.addClientCertificate(client_crt, client_key);
 
-  // check for the WiFi module:
+  // Check for the WiFi module:
   if (WiFi.status() == WL_NO_MODULE) {
     Serial.println("Communication with WiFi module failed!");
-    // don't continue
+    // Don't continue
     while (true);
   }
 
@@ -68,64 +72,64 @@ void setup() {
     Serial.println("Please upgrade the firmware");
   }
 
-  // attempt to connect to WiFi network:
+  // Attempt to connect to Wifi network:
   while (status != WL_CONNECTED) {
     Serial.print("Attempting to connect to SSID: ");
     Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-    status = WiFi.begin(ssid, pass);
+    // Connect to WPA2 enterptise network (shows as an open network).
+    status = WiFi.beginEnterprise(ssid, SECRET_USERNAME, SECRET_PASSWORD);
 
-    // wait 10 seconds for connection:
-    delay(10000);
+    // Wait 1 second for connection:
+    delay(1000);
   }
-  Serial.println("Connected to WiFi");
+  Serial.println("Connected to wifi");
   printWifiStatus();
 
   Serial.println("\nStarting connection to server...");
-  // if you get a connection, report back via serial:
+  // If you get a connection, report back via serial:
   if (client.connect(server, 80)) {
     Serial.println("connected to server");
     // Make a HTTP request:
-    client.println("GET /search?q=arduino HTTP/1.1");
-    client.println("Host: www.google.com");
+    client.println("GET /asciilogo.txt HTTP/1.1");
+    client.println("Host: www.arduino.cc");
     client.println("Connection: close");
     client.println();
   }
 }
 
 void loop() {
-  // if there are incoming bytes available
+  // If there are incoming bytes available
   // from the server, read them and print them:
   while (client.available()) {
     char c = client.read();
     Serial.write(c);
   }
 
-  // if the server's disconnected, stop the client:
+  // If the server's disconnected, stop the client:
   if (!client.connected()) {
     Serial.println();
     Serial.println("disconnecting from server.");
     client.stop();
 
-    // do nothing forevermore:
+    // Do nothing forevermore:
     while (true);
   }
 }
 
 
 void printWifiStatus() {
-  // print the SSID of the network you're attached to:
+  // Print the SSID of the network you're attached to:
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
 
-  // print your board's IP address:
+  // Print your board's IP address:
   IPAddress ip = WiFi.localIP();
   Serial.print("IP Address: ");
   Serial.println(ip);
 
-  // print the received signal strength:
+  // Print the received signal strength:
   long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI): ");
+  Serial.print("signal strength (RSSI):");
   Serial.print(rssi);
   Serial.println(" dBm");
 }
