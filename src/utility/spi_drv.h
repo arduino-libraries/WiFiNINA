e83b54117452a4a63815639e73c3d54596c8ff2d
Alexander Entinger <cto@lxrobotics.com>
2021-03-16 08:33:29 +0100

    Bugfix: Feed watchdog within busy-wait-loop within connectBearSSL to prevent a premature reset. Within the Arduino IoT Cloud firmware stack we are using a watchdog to prevent unintended application lockup. Unfortunately in certain situations the connection attempt undertaken within WiFiNINA can take so long that the watchdog is triggered although everything is fine. This change is feeding the watchdog (if one is present) so to avoid prematurely triggering of the watchdog.

--- src/utility/spi_drv.h
+++ src/utility/spi_drv.h
@@ -59,7 +59,7 @@ public:
     
     static char spiTransfer(volatile char data);
 
-    static void waitForSlaveReady();
+    static void waitForSlaveReady(bool const feed_watchdog = false);
 
     //static int waitSpiChar(char waitChar, char* readChar);
 
