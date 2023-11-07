d49c0fae549a097f07dade7c589504e272eff33b
Hasenradball <mail@frankhaefele.de>
2023-11-07 23:30:27 +0100

    Update ConnectWithWPA.ino

--- examples/ConnectWithWPA/ConnectWithWPA.ino
+++ examples/ConnectWithWPA/ConnectWithWPA.ino
@@ -13,8 +13,8 @@
 #include "arduino_secrets.h"
 
 // ==> please enter your sensitive data in the Secret tab/arduino_secrets.h
-char ssid[] = MY_SSID;        // your network SSID (name)
-char pass[] = MY_PASSWORD;    // your network password (use for WPA, or use as key for WEP)
+char ssid[] = SECRET_SSID;        // your network SSID (name)
+char pass[] = SECRET_PASS;    // your network password (use for WPA, or use as key for WEP)
 
 void setup() {
   //Initialize serial and wait for port to open:
